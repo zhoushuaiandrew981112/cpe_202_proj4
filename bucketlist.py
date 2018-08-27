f02fce8cb65840fb01b1286ec9a3d4112996b388
Zhoushuai Wu <zwu17@unix2.csc.calpoly.edu>
2018-08-26 17:23:57 -0700

    upload initiation

---
+++ bucketlist.py
@@ -0,0 +1,5 @@
+# Name:          Zhoushuai(Andrew) Wu
+# Course:        CPE 202
+# Instructor:    Daniel Kauffman
+# Assignment:    Proj 4: Bucket List
+# Term:          Summer 2018
---
+++ test.py
@@ -0,0 +1,12 @@
+from bucketlist import *
+from math import *
+import unittest
+
+
+class TestBucketeList(unittest.TestCase):
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
